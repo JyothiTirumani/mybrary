9f665fc72771e1c19dc5d319df995217fc06d7f3
jyothi t <jyothit@jyothis-mbp.lan>
2022-02-06 23:23:05 +0530

    Initial setup

---
+++ server.js
@@ -0,0 +1,30 @@
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').parse()
+// }
+
+const express= require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views' )
+app.set('layout','layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://127.0.0.1:27017/newDB', {useNewUrlParser: true})
+.catch(e => console.error('Connection error', e.message));
+//const db = MongoClient.connection
+// const mongoose = require('mongoose')
+// mongoose.connect('mongodb://127.0.0.1:27017/newDB', {useNewUrlParser: true})
+// .catch(e => console.error('Connection error', e.message));
+// const db = mongoose.connection
+//db.once('open', () => console.log('Connected to Mongoose'))
+
+app.use('/', indexRouter)
+app.listen(3000)
+
+
+
+
